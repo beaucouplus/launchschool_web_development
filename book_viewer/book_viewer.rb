64fa125d3a10660d899ed2230929777a68f80d35
beaucouplus <maxime@beaucouplus.com>
2018-08-01 12:37:28 +0200

    advanced in book_viewer exercise

--- book_viewer/book_viewer.rb
+++ book_viewer/book_viewer.rb
@@ -4,12 +4,14 @@ require "tilt/erubis"
 
 get "/" do
   @contents = File.readlines("data/toc.txt")
+  @title = "The adventures of Sherlock Holmes"
   erb :home
 end
 
-get "/chapters/:number" do |n|
-  @title = "Chapter #{n}"
+get "/chapters/:number" do 
+  @number = params[:number]
   @contents = File.readlines("data/toc.txt")
-  @chapter = File.read("data/chp#{n}.txt")
+  @title = @contents[@number.to_i - 1]
+  @chapter = File.read("data/chp#{@number}.txt")
   erb :chapter
 end
