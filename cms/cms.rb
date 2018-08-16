972b763a102b9d0cf42e19ff2f4686e58a34da7a
beaucouplus <maxime@beaucouplus.com>
2018-08-16 13:22:47 +0200

    continuing project cms

--- cms/cms.rb
+++ cms/cms.rb
@@ -1,13 +1,43 @@
-require "sinatra"
-require "sinatra/reloader"
-require "tilt/erubis"
 require 'find'
+require 'pry'
+require 'redcarpet'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'tilt/erubis'
 
 configure do
   enable :sessions
   set :session_secret, 'secret'
 end
 
+
+helpers do
+  def markdown(string)
+    headers["Content-Type"] = "text/html;charset=utf-8"
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    markdown.render(string)
+  end
+
+end
+
+def display_raw_content(file)
+  File.readlines("data/#{file}").join
+end
+
+def display_content(file)
+  raw_content = display_raw_content(file)
+  if File.extname(file) == ".md"
+    @content = markdown(raw_content)
+    erb :content
+  else
+    headers["Content-Type"] = "text/plain"
+    if session[:success]
+      session.delete(:success) + "\n\n" + File.readlines("data/#{file}").join("\n")
+    else
+      File.readlines("data/#{file}")
+    end
+  end
+end
 
 set :public_folder, "public"
 
@@ -16,15 +46,30 @@ get "/" do
 end
 
 get "/data/:file" do
+  file = params[:file]
   data_dir = Dir.pwd + "/data"
   files = Dir.entries(data_dir)
-  if files.include?(params[:file])
-    headers["Content-Type"] = "text/plain"
-    File.readlines("data/#{params[:file]}")
+  if files.include?(file)
+    display_content(file)
   else
     session[:failure] = "#{params[:file]} does not exist."
     redirect "/index"
   end
+end
+
+get "/data/:file/edit" do
+  @file = params[:file]
+  @content = display_raw_content(@file)
+  erb :edit
+end
+
+post "/data/:file" do
+  content = params[:content]
+  file = params[:file]
+  data_dir = Dir.pwd + "/data/"
+  File.open("#{data_dir}#{file}", 'w') { |f| f.write(content) }
+  session[:success] = "#{file} has been successfully updated"
+  redirect "/data/#{file}"
 end
 
 get "/index" do
--- cms/test/cms_test.rb
+++ cms/test/cms_test.rb
@@ -32,8 +32,8 @@ class AppTest < Minitest::Test
     get "/data/about.txt"
     assert_equal 200, last_response.status
     assert_equal "text/plain", last_response["Content-Type"]
-    content = "How much wood\nwould be woodchuck chuck\nif a woodchuck\ncould chuck wood"
-    assert_equal content, last_response.body
+    content = "How much wood"
+    assert_includes last_response.body, content 
   end
 
   def test_show_does_not_exist_when_no_file
@@ -46,6 +46,38 @@ class AppTest < Minitest::Test
     get last_response["Location"] # Request the page that the user was redirected to
     assert_equal 200, last_response.status
     assert_includes(last_response.body, "does not exist")
+  end
+
+  # test/cms_test.rb
+  def test_viewing_markdown_document
+    get "/data/about.md"
+
+    assert_equal 200, last_response.status
+    assert_equal "text/html;charset=utf-8", last_response["Content-Type"]
+    assert_includes last_response.body, "<h1>How much wood</h1>"
+  end
+
+  # test/cms_test.rb
+  def test_editing_document
+    get "/data/changes.txt/edit"
+
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "<textarea"
+    assert_includes last_response.body, %q(<button type="submit")
+  end
+
+  def test_updating_document
+    post "/data/changes.txt", content: "new content"
+
+    assert_equal 302, last_response.status
+
+    get last_response["Location"]
+
+    assert_includes last_response.body, "changes.txt has been successfully updated"
+
+    get "/data/changes.txt"
+    assert_equal 200, last_response.status
+    assert_includes last_response.body, "new content"
   end
 
 end
